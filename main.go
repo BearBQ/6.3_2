f32c3f6592fd6c32a24392e886bd60b1b9f78b60
Dmitry <d.a.demkin@gmail.com>
2025-07-14 19:59:37 +0500

    finish

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func okHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("200 ok"))
+}
+
+func panicHandler(w http.ResponseWriter, r *http.Request) {
+	panic("что-то не так")
+}
+
+func recoverMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic caught: %v", err)
+				http.Error(w, "Внутренняя ошибка сервера", http.StatusInternalServerError)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	http.Handle("/panic", recoverMiddleware(http.HandlerFunc(panicHandler)))
+	http.Handle("/ok", recoverMiddleware(http.HandlerFunc(okHandler)))
+	http.ListenAndServe(":8080", nil)
+}
